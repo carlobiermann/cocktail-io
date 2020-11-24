a3c20d1476818cb8275243820d1c323ca6538c8f
carlobiermann <carlo.biermann@protonmail.com>
2020-11-24 18:53:56 +0100

    more GUI pages

--- rpi/05_main/main.py
+++ rpi/05_main/main.py
@@ -109,7 +109,7 @@ class PageTwo(tk.Frame):
         self.btn2 = tk.Button(master)
         self.btn2.configure(text="Weiter", font=('Arial', 12), borderwidth="0")
         self.btn2.configure(bg="#216870", activebackground="#ff6666", foreground="#ffffff")
-        self.btn2.configure(command=lambda:master.switch_frame(PageOne))
+        self.btn2.configure(command=lambda:master.switch_frame(PageThree))
        
     def routineStart(self):
         stand = int(self.lbl2.cget('text'))
@@ -123,6 +123,175 @@ class PageTwo(tk.Frame):
             self.btn2.place(relx=0.331, rely=0.731, height=71, width=201)
             self.lbl2.configure(text="Messung fertig", font=('Arial',24))
 
+class PageThree(tk.Frame):    
+    def __init__(self, master):
+        tk.Frame.__init__(self, master) 
+        tk.Frame.place(self, relx=0.0, rely=0.0, relheight=1, relwidth=1)
+        tk.Frame.configure(self, background="#212121")
+
+        lbl = tk.Label(self)
+        lbl.configure(text="Wie ist dein Pegel?", font=('Arial', 32, "bold"), bg="#212121")
+        lbl.configure(foreground="#ffffff")
+        lbl.place(relx=0.066, rely=0.088, height=83, width=519)
+
+        Beschreibung = "Finde heraus wie viel Alkohol du schon intus hast und\npuste in das Roehrchen neben der Kamera."
+
+        T = tk.Text(self)
+        T.configure(font=('Arial',12), bg="#212121", borderwidth="0")
+        T.configure(foreground="#ffffff")
+        T.configure(wrap="word")
+        T.insert("1.0", Beschreibung)
+        T.place(relx=0.083, rely=0.329, relheight=0.34, relwidth=0.821)
+
+        btn = tk.Button(self)
+        btn.configure(text="Weiter", font=('Arial', 12), borderwidth="0")
+        btn.configure(bg="#212121", activebackground="#ff6666", foreground="#ffffff")
+        btn.configure(command=lambda:master.switch_frame(PageFour))
+        btn.place(relx=0.331, rely=0.731, height=71, width=201)
+
+class PageFour(tk.Frame):    
+    def __init__(self, master):
+        tk.Frame.__init__(self, master) 
+        tk.Frame.place(self, relx=0.0, rely=0.0, relheight=1, relwidth=1)
+        tk.Frame.configure(self, background="#212121")
+
+        lbl1 = tk.Label(self)
+        lbl1.configure(text="Druecke auf 'Bereit' und puste nach dem Countdown in das Roehrchen.", font=('Arial', 12, "bold"), bg="#212121")
+        lbl1.configure(foreground="#ffffff")
+        lbl1.place(relx=0.066, rely=0.172, height=83, width=519)
+
+        self.lbl2 = tk.Label(master)
+        self.lbl2.configure(activebackground="#f9f9f9")
+        self.lbl2.configure(background="#8e8e8e")
+        self.lbl2.configure(foreground="#ff6666")
+        self.lbl2.configure(font=('Arial',60), borderwidth="0")
+        self.lbl2.configure(text="3")
+        self.lbl2.place(relx=0.310, rely=0.409, height=141, width=220)
+
+        self.btn = tk.Button(master)
+        self.btn.configure(text="Bereit!", font=('Arial', 12), borderwidth="0")
+        self.btn.configure(bg="#212121", activebackground="#ff6666", foreground="#ffffff")
+        self.btn.configure(command=self.routineStart)
+        self.btn.place(relx=0.331, rely=0.731, height=71, width=201)
+
+        self.btn2 = tk.Button(master)
+        self.btn2.configure(text="Weiter", font=('Arial', 12), borderwidth="0")
+        self.btn2.configure(bg="#216870", activebackground="#ff6666", foreground="#ffffff")
+        self.btn2.configure(command=lambda:master.switch_frame(PageFive))
+       
+    def routineStart(self):
+        stand = int(self.lbl2.cget('text'))
+        if stand > 0:
+            stand = stand - 1
+            self.lbl2.configure(text=str(stand))
+            self.lbl2.after(1000, self.routineStart)
+        elif stand == 0:
+            # ARDUINO MEASUREMENT
+            self.btn.place_forget()
+            self.btn2.place(relx=0.331, rely=0.731, height=71, width=201)
+            self.lbl2.configure(text="Messung fertig", font=('Arial',24))
+
+
+class PageFive(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master) 
+        tk.Frame.place(self, relx=0.0, rely=0.0, relheight=1, relwidth=1)
+        tk.Frame.configure(self, background="#212121")
+
+        lbl = tk.Label(self)
+        lbl.configure(text="Ergebnis", font=('Arial', 32, "bold"), bg="#212121")
+        lbl.configure(foreground="#ffffff")
+        lbl.place(relx=0.066, rely=0.088, height=83, width=519)
+
+        Stimmung = "heiter"
+        StimmungText = "Deine Stimmung ist " + Stimmung + ".\n"
+        
+        Pegel = "Einer geht noch!"
+        PegelText = "Dein Pegel sagt: '" + Pegel + "'\n"
+        
+        Beschreibung = StimmungText + PegelText + "Diese Drinks passen jetzt am besten zu dir.\nWaehle einen aus."
+
+        T = tk.Text(self)
+        T.configure(font=('Arial',12), bg="#212121", borderwidth="0")
+        T.configure(foreground="#ffffff")
+        T.configure(wrap="word")
+        T.insert("1.0", Beschreibung)
+        T.place(relx=0.182, rely=0.329, relheight=0.211, relwidth=0.64)
+
+        auswahlA = "Auswahl A"
+        auswahlB = "Auswahl B"
+        auswahlC = "Auswahl C"
+
+        self.btn1 = tk.Button(master)
+        self.btn1.configure(text=auswahlA, font=('Arial', 12), borderwidth="0")
+        self.btn1.configure(bg="#212121", activebackground="#ff6666", foreground="#ffffff")
+        self.btn1.configure(command=self.drinkA)
+        self.btn1.place(relx=0.083, rely=0.645, height=71, width=141)
+ 
+        self.btn2 = tk.Button(master)
+        self.btn2.configure(text=auswahlB, font=('Arial', 12), borderwidth="0")
+        self.btn2.configure(bg="#212121", activebackground="#ff6666", foreground="#ffffff")
+        self.btn2.configure(command=self.drinkB)
+        self.btn2.place(relx=0.38, rely=0.645, height=71, width=141)
+
+        self.btn3 = tk.Button(master)
+        self.btn3.configure(text=auswahlC, font=('Arial', 12), borderwidth="0")
+        self.btn3.configure(bg="#212121", activebackground="#ff6666", foreground="#ffffff")
+        self.btn3.configure(command=self.drinkC)
+        self.btn3.place(relx=0.678, rely=0.645, height=71, width=141)
+    
+        self.btn4 = tk.Button(master)
+        self.btn4.configure(text="Weiter", font=('Arial', 12), borderwidth="0")
+        self.btn4.configure(bg="#216870", activebackground="#ff6666", foreground="#ffffff")
+        self.btn4.configure(command=lambda:master.switch_frame(PageSix))
+
+    def drinkA(self):
+        self.btn1.place_forget()
+        self.btn2.place_forget()
+        self.btn3.place_forget()
+        self.btn4.place(relx=0.38, rely=0.645, height=71, width=141)
+        print("Drink A")
+        #SEND TO ARDUINO 
+    
+    def drinkB(self): 
+        self.btn1.place_forget()
+        self.btn2.place_forget()
+        self.btn3.place_forget()
+        self.btn4.place(relx=0.38, rely=0.645, height=71, width=141)
+        print("Drink B")
+        #SEND TO ARDUINO
+
+    def drinkC(self):
+        self.btn1.place_forget()
+        self.btn2.place_forget()
+        self.btn3.place_forget()
+        self.btn4.place(relx=0.38, rely=0.645, height=71, width=141)
+        print("Drink C")
+        #SEND TO ARDUINO
+
+class PageSix(tk.Frame):    
+    def __init__(self, master):
+        tk.Frame.__init__(self, master) 
+        tk.Frame.place(self, relx=0.0, rely=0.0, relheight=1, relwidth=1)
+        tk.Frame.configure(self, background="#212121")
+
+        lbl1 = tk.Label(self)
+        lbl1.configure(text="Dein Drink wird zubereitet", font=('Arial', 12, "bold"), bg="#212121")
+        lbl1.configure(foreground="#ffffff")
+        lbl1.place(relx=0.066, rely=0.237, height=43, width=519)
+
+        lbl2 = tk.Label(self)
+        lbl2.configure(text="Have Fun!", font=('Arial', 12, "bold"), bg="#212121")
+        lbl2.configure(foreground="#ffffff")
+        lbl2.place(relx=0.066, rely=0.366, height=43, width=519)
+
+        self.btn = tk.Button(master)
+        self.btn.configure(text="Danke", font=('Arial', 12), borderwidth="0")
+        self.btn.configure(bg="#212121", activebackground="#ff6666", foreground="#ffffff")
+        self.btn.configure(command=lambda:master.switch_frame(StartPage))
+        self.btn.place(relx=0.331, rely=0.731, height=71, width=201)
+
 if __name__ == "__main__":
     app = SampleApp()
     app.mainloop()
+
