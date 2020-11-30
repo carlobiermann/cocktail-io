4308937ce2d5217e4c287ec5e52dbabdf5d62c46
carlobiermann <carlo.biermann@protonmail.com>
2020-11-30 20:36:03 +0100

    frontend finish

--- rpi/05_main/main.py
+++ rpi/05_main/main.py
@@ -36,7 +36,7 @@ class StartPage(tk.Frame):
         tk.Frame.configure(self, background="#212121")
 
         lbl = tk.Label(self)
-        lbl.configure(text="Der Getrnk", font=('Arial', 32, "bold"), bg="#212121")
+        lbl.configure(text="Der Getränk", font=('Arial', 32, "bold"), bg="#212121")
         lbl.configure(foreground="#ffffff")
         lbl.place(relx=0.066, rely=0.088, height=83, width=519)
 
@@ -60,7 +60,7 @@ class StartPage(tk.Frame):
 
         zeile1 = "Ich bin dein Barkeeper 4.0"
         zeile2 = "Anhand deiner Stimmung und deinem Pegel"
-        zeile3 = "weiss ich, welchen Drink du jetzt brauchst."
+        zeile3 = "weiß ich, welchen Drink du jetzt brauchst."
 
         self.lbl1.configure(text=zeile1, font=('Arial', 16, "bold"), bg="#212121")
         self.lbl2.configure(text=zeile2, font=('Arial', 16, "bold"), bg="#212121")
@@ -96,7 +96,7 @@ class PageOne(tk.Frame):
         self.lbl2.place(relx=0.066, rely=0.43, height=43, width=519)
 
         zeile1 = "Zeig mir deine aktuelle Stimmung."
-        zeile2 = "Die Kamera befindet sich ueber dem Display."
+        zeile2 = "Die Kamera befindet sich über dem Display."
 
         self.lbl1.configure(text=zeile1, font=('Arial', 16, "bold"), bg="#212121")
         self.lbl2.configure(text=zeile2, font=('Arial', 16, "bold"), bg="#212121")
@@ -114,7 +114,7 @@ class PageTwo(tk.Frame):
         tk.Frame.configure(self, background="#212121")
 
         lbl1 = tk.Label(self)
-        lbl1.configure(text="Druecke auf 'Bereit' und schaue nach dem Countdown in die Kamera", font=('Arial', 12, "bold"), bg="#212121")
+        lbl1.configure(text="Drücke auf 'Bereit' und schaue nach dem Countdown in die Kamera", font=('Arial', 12, "bold"), bg="#212121")
         lbl1.configure(foreground="#ffffff")
         lbl1.place(relx=0.017, rely=0.172, height=83, width=579)
 
@@ -173,7 +173,7 @@ class PageThree(tk.Frame):
         self.lbl2.place(relx=0.066, rely=0.43, height=43, width=519)
 
         zeile1 = "Finde heraus wie viel Alkohol du schon intus hast"
-        zeile2 = "und puste in das Roehrchen neben der Kamera."
+        zeile2 = "und puste in das Röhrchen neben der Kamera."
 
         self.lbl1.configure(text=zeile1, font=('Arial', 16, "bold"), bg="#212121")
         self.lbl2.configure(text=zeile2, font=('Arial', 16, "bold"), bg="#212121")
@@ -191,7 +191,7 @@ class PageFour(tk.Frame):
         tk.Frame.configure(self, background="#212121")
 
         lbl1 = tk.Label(self)
-        lbl1.configure(text="Druecke auf 'Bereit' und puste nach dem Countdown in das Roehrchen.", font=('Arial', 12, "bold"), bg="#212121")
+        lbl1.configure(text="Drücke auf 'Bereit' und puste nach dem Countdown in das Röhrchen.", font=('Arial', 12, "bold"), bg="#212121")
         lbl1.configure(foreground="#ffffff")
         lbl1.place(relx=0.017, rely=0.172, height=83, width=579)
 
@@ -236,23 +236,48 @@ class PageFive(tk.Frame):
         lbl = tk.Label(self)
         lbl.configure(text="Ergebnis", font=('Arial', 32, "bold"), bg="#212121")
         lbl.configure(foreground="#ffffff")
-        lbl.place(relx=0.066, rely=0.088, height=83, width=519)
+        lbl.place(relx=0.017, rely=0.043, height=73, width=579)
 
         Stimmung = "heiter"
-        StimmungText = "Deine Stimmung ist " + Stimmung + ".\n"
+        StimmungText = "Deine Stimmung ist " + Stimmung + "."
         
         Pegel = "Einer geht noch!"
-        PegelText = "Dein Pegel sagt: '" + Pegel + "'\n"
+        PegelText = "Dein Pegel sagt: '" + Pegel + "'"
         
-        Beschreibung = StimmungText + PegelText + "Diese Drinks passen jetzt am besten zu dir.\nWaehle einen aus."
+        self.lbl1 = tk.Label(master)
+        self.lbl1.configure(background="#212121")
+        self.lbl1.configure(borderwidth="0")
+        self.lbl1.configure(foreground="#ffffff")
+        self.lbl1.place(relx=0.017, rely=0.237, height=33, width=579)
 
-        T = tk.Text(self)
-        T.configure(font=('Arial',12), bg="#212121", borderwidth="0")
-        T.configure(foreground="#ffffff")
-        T.configure(wrap="word")
-        T.insert("1.0", Beschreibung)
-        T.place(relx=0.182, rely=0.329, relheight=0.211, relwidth=0.64)
+        self.lbl2 = tk.Label(master)
+        self.lbl2.configure(background="#212121")
+        self.lbl2.configure(borderwidth="0")
+        self.lbl2.configure(foreground="#ffffff")
+        self.lbl2.place(relx=0.017, rely=0.323, height=33, width=579)
+        
+        self.lbl3 = tk.Label(master)
+        self.lbl3.configure(background="#212121")
+        self.lbl3.configure(borderwidth="0")
+        self.lbl3.configure(foreground="#ffffff")
+        self.lbl3.place(relx=0.017, rely=0.409, height=33, width=579)
 
+        self.lbl4 = tk.Label(master)
+        self.lbl4.configure(background="#212121")
+        self.lbl4.configure(borderwidth="0")
+        self.lbl4.configure(foreground="#ffffff")
+        self.lbl4.place(relx=0.017, rely=0.774, height=33, width=579)
+        
+        zeile1 = StimmungText
+        zeile2 = PegelText
+        zeile3 = "Diese Drinks passen jetzt am besten zu dir:"
+        zeile4 = "Wähle einen aus."
+
+        self.lbl1.configure(text=zeile1, font=('Arial', 16, "bold"), bg="#212121")
+        self.lbl2.configure(text=zeile2, font=('Arial', 16, "bold"), bg="#212121")
+        self.lbl3.configure(text=zeile3, font=('Arial', 16, "bold"), bg="#212121")
+        self.lbl4.configure(text=zeile4, font=('Arial', 16, "bold"), bg="#212121")
+        
         auswahlA = "Auswahl A"
         auswahlB = "Auswahl B"
         auswahlC = "Auswahl C"
@@ -261,19 +286,19 @@ class PageFive(tk.Frame):
         self.btn1.configure(text=auswahlA, font=('Arial', 12, "bold"), borderwidth="0")
         self.btn1.configure(bg="#212121", activebackground="#ff6666", foreground="#ffffff")
         self.btn1.configure(command=self.drinkA)
-        self.btn1.place(relx=0.083, rely=0.645, height=71, width=141)
+        self.btn1.place(relx=0.083, rely=0.559, height=71, width=141)
  
         self.btn2 = tk.Button(master)
         self.btn2.configure(text=auswahlB, font=('Arial', 12, "bold"), borderwidth="0")
         self.btn2.configure(bg="#212121", activebackground="#ff6666", foreground="#ffffff")
         self.btn2.configure(command=self.drinkB)
-        self.btn2.place(relx=0.38, rely=0.645, height=71, width=141)
+        self.btn2.place(relx=0.38, rely=0.559, height=71, width=141)
 
         self.btn3 = tk.Button(master)
         self.btn3.configure(text=auswahlC, font=('Arial', 12, "bold"), borderwidth="0")
         self.btn3.configure(bg="#212121", activebackground="#ff6666", foreground="#ffffff")
         self.btn3.configure(command=self.drinkC)
-        self.btn3.place(relx=0.678, rely=0.645, height=71, width=141)
+        self.btn3.place(relx=0.678, rely=0.559, height=71, width=141)
     
         self.btn4 = tk.Button(master)
         self.btn4.configure(text="Weiter", font=('Arial', 12, "bold"), borderwidth="0")
@@ -284,7 +309,7 @@ class PageFive(tk.Frame):
         self.btn1.place_forget()
         self.btn2.place_forget()
         self.btn3.place_forget()
-        self.btn4.place(relx=0.38, rely=0.645, height=71, width=141)
+        self.btn4.place(relx=0.38, rely=0.559, height=71, width=141)
         print("Drink A")
         #SEND TO ARDUINO 
     
@@ -292,7 +317,7 @@ class PageFive(tk.Frame):
         self.btn1.place_forget()
         self.btn2.place_forget()
         self.btn3.place_forget()
-        self.btn4.place(relx=0.38, rely=0.645, height=71, width=141)
+        self.btn4.place(relx=0.38, rely=0.559, height=71, width=141)
         print("Drink B")
         #SEND TO ARDUINO
 
@@ -300,7 +325,7 @@ class PageFive(tk.Frame):
         self.btn1.place_forget()
         self.btn2.place_forget()
         self.btn3.place_forget()
-        self.btn4.place(relx=0.38, rely=0.645, height=71, width=141)
+        self.btn4.place(relx=0.38, rely=0.559, height=71, width=141)
         print("Drink C")
         #SEND TO ARDUINO
 
@@ -311,12 +336,12 @@ class PageSix(tk.Frame):
         tk.Frame.configure(self, background="#212121")
 
         lbl1 = tk.Label(self)
-        lbl1.configure(text="Dein Drink wird zubereitet", font=('Arial', 12, "bold"), bg="#212121")
+        lbl1.configure(text="Dein Drink wird zubereitet", font=('Arial', 16, "bold"), bg="#212121")
         lbl1.configure(foreground="#ffffff")
         lbl1.place(relx=0.066, rely=0.237, height=43, width=519)
 
         lbl2 = tk.Label(self)
-        lbl2.configure(text="Have Fun!", font=('Arial', 12, "bold"), bg="#212121")
+        lbl2.configure(text="Have Fun!", font=('Arial', 16, "bold"), bg="#212121")
         lbl2.configure(foreground="#ffffff")
         lbl2.place(relx=0.066, rely=0.366, height=43, width=519)
 
