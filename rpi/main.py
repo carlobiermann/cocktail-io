a9199428589b7e76b2a87d7a41d722feaf20b8e3
carlobiermann <carlo.biermann@protonmail.com>
2020-12-10 19:59:57 +0100

    GUI rescale for touch display

--- rpi/main.py
+++ rpi/main.py
@@ -24,7 +24,7 @@ class barkeeperApp(tk.Tk):
         self.attributes("-fullscreen", True)
         self.bind("<Escape>", self.endFullscreen)
     
-        self.geometry("800x480")
+        self.geometry("800x480+0+0")
         self.title("Barkeeper 4.0")
         self.configure(bg="#212121")
         self.switchFrame(pageOne)
@@ -50,19 +50,19 @@ class pageOne(tk.Frame):
         self.heading = tk.Label(master)
         self.heading.configure(text="Der Getränk", font=('Arial', 32, "bold"))
         self.heading.configure(bg="#212121", fg="#ffffff")
-        self.heading.place(relx=0.066, rely=0.088, height=83, width=519)
+        self.heading.place(relx=0.013, rely=0.042, height=101, width=780)
 
         self.lbl1 = tk.Label(master)
         self.lbl1.configure(bg="#212121", fg="#ffffff")
-        self.lbl1.place(relx=0.066, rely=0.344, height=43, width=519)
+        self.lbl1.place(relx=0.013, rely=0.313, height=51, width=780)
 
         self.lbl2 = tk.Label(master)
         self.lbl2.configure(bg="#212121", fg="#ffffff")
-        self.lbl2.place(relx=0.066, rely=0.43, height=43, width=519)
+        self.lbl2.place(relx=0.013, rely=0.417, height=51, width=780)
         
         self.lbl3 = tk.Label(master)
         self.lbl3.configure(bg="#212121", fg="#ffffff")
-        self.lbl3.place(relx=0.066, rely=0.516, height=43, width=519)
+        self.lbl3.place(relx=0.013, rely=0.521, height=51, width=780)
 
         row1 = "Ich bin dein Barkeeper 4.0"
         row2 = "Anhand deiner Stimmung und deinem Pegel"
@@ -77,7 +77,7 @@ class pageOne(tk.Frame):
         self.btn.configure(bg="#212121",activebackground="#ff6666")
         self.btn.configure(foreground="#ffffff")
         self.btn.configure(command=lambda: master.switchFrame(pageTwo))
-        self.btn.place(relx=0.331, rely=0.731, height=71, width=201)
+        self.btn.place(relx=0.338, rely=0.688, height=81, width=261)
 
 class pageTwo(tk.Frame):    
     def __init__(self, master):
@@ -88,15 +88,15 @@ class pageTwo(tk.Frame):
         self.heading = tk.Label(master)
         self.heading.configure(text="Wie ist deine Laune?", font=('Arial', 32, "bold"))
         self.heading.configure(bg="#212121", fg="#ffffff")
-        self.heading.place(relx=0.066, rely=0.088, height=83, width=519)
+        self.heading.place(relx=0.013, rely=0.042, height=101, width=780)
 
         self.lbl1 = tk.Label(master)
         self.lbl1.configure(bg="#212121", fg="#ffffff")
-        self.lbl1.place(relx=0.066, rely=0.344, height=43, width=519)
+        self.lbl1.place(relx=0.013, rely=0.313, height=51, width=780)
 
         self.lbl2 = tk.Label(master)
         self.lbl2.configure(bg="#212121", fg="#ffffff")
-        self.lbl2.place(relx=0.066, rely=0.43, height=43, width=519)
+        self.lbl2.place(relx=0.013, rely=0.417, height=51, width=780)
 
         row1 = "Zeig mir deine aktuelle Stimmung."
         row2 = "Die Kamera befindet sich über dem Display."
@@ -108,7 +108,7 @@ class pageTwo(tk.Frame):
         self.btn.configure(text="Weiter", font=('Arial', 12, "bold"))
         self.btn.configure(bg="#212121", activebackground="#ff6666", fg="#ffffff")
         self.btn.configure(command=lambda:master.switchFrame(pageThree))
-        self.btn.place(relx=0.331, rely=0.731, height=71, width=201)
+        self.btn.place(relx=0.338, rely=0.688, height=81, width=261)
 
 class pageThree(tk.Frame):    
     def __init__(self, master):
@@ -118,14 +118,14 @@ class pageThree(tk.Frame):
 
         self.heading = tk.Label(master)
         self.heading.configure(text="Drücke auf 'Bereit' und schaue nach dem Countdown in die Kamera")
-        self.heading.configure(font=('Arial', 12, "bold"))
+        self.heading.configure(font=('Arial', 16, "bold"))
         self.heading.configure(bg="#212121", fg="#ffffff")
-        self.heading.place(relx=0.017, rely=0.172, height=83, width=579)
+        self.heading.place(relx=0.013, rely=0.146, height=51, width=780)
 
         self.lbl = tk.Label(master)
         self.lbl.configure(text="3", font=('Arial',60))
         self.lbl.configure(bg="#212121", fg="#ff6666")
-        self.lbl.place(relx=0.198, rely=0.344, height=141, width=350)
+        self.lbl.place(relx=0.013, rely=0.271, height=181, width=780)
 
         # non visible counter to keep track of time while decr. counter inside routineStart()
         self.counter = tk.IntVar()
@@ -135,7 +135,7 @@ class pageThree(tk.Frame):
         self.btn1.configure(text="Bereit!", font=('Arial', 12, "bold"))
         self.btn1.configure(bg="#212121", activebackground="#ff6666", fg="#ffffff")
         self.btn1.configure(command=self.startRoutine)
-        self.btn1.place(relx=0.331, rely=0.731, height=71, width=201)
+        self.btn1.place(relx=0.338, rely=0.688, height=81, width=261)
 
         self.btn2 = tk.Button(master)
         self.btn2.configure(text="Weiter", font=('Arial', 12, "bold"))
@@ -160,7 +160,7 @@ class pageThree(tk.Frame):
             emoData = emotions.emotionDetection()
             print(emoData)
             self.btn1.place_forget()
-            self.btn2.place(relx=0.331, rely=0.731, height=71, width=201)
+            self.btn2.place(relx=0.338, rely=0.688, height=81, width=261)
             self.lbl.configure(text="Messung fertig")
 
 class pageFour(tk.Frame):    
@@ -173,15 +173,15 @@ class pageFour(tk.Frame):
         self.heading.configure(text="Wie ist dein Pegel?")
         self.heading.configure(font=('Arial', 32, "bold"))
         self.heading.configure(bg="#212121", fg="#ffffff")
-        self.heading.place(relx=0.066, rely=0.088, height=83, width=519)
+        self.heading.place(relx=0.013, rely=0.042, height=101, width=780)
 
         self.lbl1 = tk.Label(master)
         self.lbl1.configure(bg="#212121", fg="#ffffff")
-        self.lbl1.place(relx=0.066, rely=0.344, height=43, width=519)
+        self.lbl1.place(relx=0.013, rely=0.313, height=51, width=780)
 
         self.lbl2 = tk.Label(master)
         self.lbl2.configure(bg="#212121", fg="#ffffff")
-        self.lbl2.place(relx=0.066, rely=0.43, height=43, width=519)
+        self.lbl2.place(relx=0.013, rely=0.417, height=51, width=780)
 
         row1 = "Finde heraus wie viel Alkohol du schon intus hast"
         row2 = "und puste in das Röhrchen neben der Kamera."
@@ -193,7 +193,7 @@ class pageFour(tk.Frame):
         self.btn.configure(text="Weiter", font=('Arial', 12, "bold"))
         self.btn.configure(bg="#212121", activebackground="#ff6666", fg="#ffffff")
         self.btn.configure(command=lambda:master.switchFrame(pageFive))
-        self.btn.place(relx=0.331, rely=0.731, height=71, width=201)
+        self.btn.place(relx=0.338, rely=0.688, height=81, width=261)
 
 class pageFive(tk.Frame):    
     def __init__(self, master):
@@ -203,20 +203,20 @@ class pageFive(tk.Frame):
 
         self.heading = tk.Label(master)
         self.heading.configure(text="Drücke auf 'Bereit' und puste nach dem Countdown in das Röhrchen.",)
-        self.heading.configure(font=('Arial', 12, "bold"))
+        self.heading.configure(font=('Arial', 16, "bold"))
         self.heading.configure(bg="#212121", fg="#ffffff")
-        self.heading.place(relx=0.017, rely=0.172, height=83, width=579)
+        self.heading.place(relx=0.013, rely=0.146, height=51, width=780)
 
         self.lbl = tk.Label(master)
         self.lbl.configure(text="3", font=('Arial',60))
         self.lbl.configure(bg="#212121", fg="#ff6666")
-        self.lbl.place(relx=0.198, rely=0.344, height=141, width=350)
+        self.lbl.place(relx=0.013, rely=0.271, height=181, width=780)
 
         self.btn1 = tk.Button(master)
         self.btn1.configure(text="Bereit!", font=('Arial', 12, "bold"))
         self.btn1.configure(bg="#212121", activebackground="#ff6666", fg="#ffffff")
         self.btn1.configure(command=self.startRoutine)
-        self.btn1.place(relx=0.331, rely=0.731, height=71, width=201)
+        self.btn1.place(relx=0.338, rely=0.688, height=81, width=261)
 
         # non visible counter to keep track of time while decr. counter inside routineStart()
         self.counter = tk.IntVar()
@@ -246,7 +246,7 @@ class pageFive(tk.Frame):
             sensorData = arduinoI2C.readSensors() 
             print(sensorData)
             self.btn1.place_forget()
-            self.btn2.place(relx=0.331, rely=0.731, height=71, width=201)
+            self.btn2.place(relx=0.338, rely=0.688, height=81, width=261)
             self.lbl.configure(text="Messung fertig")
             
             # send all data to NN
@@ -271,7 +271,7 @@ class pageSix(tk.Frame):
         self.heading = tk.Label(master)
         self.heading.configure(text="Ergebnis", font=('Arial', 32, "bold"))
         self.heading.configure(bg="#212121", fg="#ffffff")
-        self.heading.place(relx=0.017, rely=0.043, height=73, width=579)
+        self.heading.place(relx=0.013, rely=0.042, height=101, width=780)
 
         # wait for return from function that analyzes emotion/alcohol data 
         # and puts them into moods and levels of intoxication as strings
@@ -286,19 +286,19 @@ class pageSix(tk.Frame):
         
         self.lbl1 = tk.Label(master)
         self.lbl1.configure(bg="#212121", fg="#ffffff")
-        self.lbl1.place(relx=0.017, rely=0.237, height=33, width=579)
+        self.lbl1.place(relx=0.013, rely=0.271, height=51, width=780)
 
         self.lbl2 = tk.Label(master)
         self.lbl2.configure(bg="#212121", fg="#ffffff")
-        self.lbl2.place(relx=0.017, rely=0.323, height=33, width=579)
+        self.lbl2.place(relx=0.013, rely=0.375, height=51, width=780)
         
         self.lbl3 = tk.Label(master)
         self.lbl3.configure(bg="#212121", fg="#ffffff")
-        self.lbl3.place(relx=0.017, rely=0.409, height=33, width=579)
+        self.lbl3.place(relx=0.013, rely=0.479, height=51, width=780)
 
         self.lbl4 = tk.Label(master)
         self.lbl4.configure(bg="#212121", fg="#ffffff")
-        self.lbl4.place(relx=0.017, rely=0.774, height=33, width=579)
+        self.lbl4.place(relx=0.013, rely=0.833, height=51, width=780)
         
         row1 = StimmungText
         row2 = PegelText
@@ -310,7 +310,7 @@ class pageSix(tk.Frame):
         self.lbl3.configure(text=row3, font=('Arial', 16, "bold"))
         self.lbl4.configure(text=row4, font=('Arial', 16, "bold"))
        
-        # dummy input from neurak network
+        # dummy input from neural network
         auswahlA = "Auswahl A"
         auswahlB = "Auswahl B"
         auswahlC = "Auswahl C"
@@ -319,19 +319,19 @@ class pageSix(tk.Frame):
         self.btn1.configure(text=auswahlA, font=('Arial', 12, "bold"))
         self.btn1.configure(bg="#212121", activebackground="#ff6666", fg="#ffffff")
         self.btn1.configure(command=self.drinkA)
-        self.btn1.place(relx=0.083, rely=0.559, height=71, width=141)
+        self.btn1.place(relx=0.05, rely=0.625, height=81, width=181)
  
         self.btn2 = tk.Button(master)
         self.btn2.configure(text=auswahlB, font=('Arial', 12, "bold"))
         self.btn2.configure(bg="#212121", activebackground="#ff6666", fg="#ffffff")
         self.btn2.configure(command=self.drinkB)
-        self.btn2.place(relx=0.38, rely=0.559, height=71, width=141)
+        self.btn2.place(relx=0.388, rely=0.625, height=81, width=181)
 
         self.btn3 = tk.Button(master)
         self.btn3.configure(text=auswahlC, font=('Arial', 12, "bold"))
         self.btn3.configure(bg="#212121", activebackground="#ff6666", fg="#ffffff")
         self.btn3.configure(command=self.drinkC)
-        self.btn3.place(relx=0.678, rely=0.559, height=71, width=141)
+        self.btn3.place(relx=0.725, rely=0.625, height=81, width=181)
     
         self.btn4 = tk.Button(master)
         self.btn4.configure(text="Weiter", font=('Arial', 12, "bold"))
@@ -342,7 +342,7 @@ class pageSix(tk.Frame):
         self.btn1.place_forget()
         self.btn2.place_forget()
         self.btn3.place_forget()
-        self.btn4.place(relx=0.38, rely=0.559, height=71, width=141)
+        self.btn3.place(relx=0.05, rely=0.625, height=81, width=181)
         print("Drink A")
         #SEND TO ARDUINO 
         arduinoI2C.sendDrink(1)
@@ -351,7 +351,7 @@ class pageSix(tk.Frame):
         self.btn1.place_forget()
         self.btn2.place_forget()
         self.btn3.place_forget()
-        self.btn4.place(relx=0.38, rely=0.559, height=71, width=141)
+        self.btn4.place(relx=0.388, rely=0.625, height=81, width=181)
         print("Drink B")
         #SEND TO ARDUINO
         arduinoI2C.sendDrink(2)
@@ -360,7 +360,7 @@ class pageSix(tk.Frame):
         self.btn1.place_forget()
         self.btn2.place_forget()
         self.btn3.place_forget()
-        self.btn4.place(relx=0.38, rely=0.559, height=71, width=141)
+        self.btn4.place(relx=0.725, rely=0.625, height=81, width=181)
         print("Drink C")
         #SEND TO ARDUINO
         arduinoI2C.sendDrink(3)
@@ -374,18 +374,18 @@ class pageSeven(tk.Frame):
         self.lbl1 = tk.Label(master)
         self.lbl1.configure(text="Dein Drink wird zubereitet", font=('Arial', 16, "bold"))
         self.lbl1.configure(bg="#212121", fg="#ffffff")
-        self.lbl1.place(relx=0.066, rely=0.237, height=43, width=519)
+        self.lbl1.place(relx=0.013, rely=0.313, height=51, width=780)
 
         self.lbl2 = tk.Label(master)
         self.lbl2.configure(text="Have Fun!", font=('Arial', 16, "bold"))
         self.lbl2.configure(bg="#212121", fg="#ffffff")
-        self.lbl2.place(relx=0.066, rely=0.366, height=43, width=519)
+        self.lbl2.place(relx=0.013, rely=0.417, height=51, width=780)
 
         self.btn = tk.Button(master)
         self.btn.configure(text="Danke", font=('Arial', 12, "bold"))
         self.btn.configure(bg="#212121", activebackground="#ff6666", fg="#ffffff")
         self.btn.configure(command=lambda:master.switchFrame(pageOne))
-        self.btn.place(relx=0.331, rely=0.731, height=71, width=201)
+        self.btn.place(relx=0.338, rely=0.688, height=81, width=261)
 
 if __name__ == "__main__":
     app = barkeeperApp()
