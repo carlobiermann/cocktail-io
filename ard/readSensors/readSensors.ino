6b8a4a75efd45fa2f532dd040fb31a4be2cb29d5
carlobiermann <carlo.biermann@protonmail.com>
2021-02-03 21:33:57 +0100

    added DHT sensor

--- ard/readSensors/readSensors.ino
+++ ard/readSensors/readSensors.ino
@@ -1,38 +1,40 @@
 #include <Wire.h>
-// #include <dht11.h>
+#include <DHT.h> //Adarfruit library Unified_Sensors muss auch installiert werden
 
 #define SLAVE_ADDRESS 0x05   // I2C-SLAVE-ADR.
 #define PIN_ANALOG_OUT  A1   // Alk. 
-// #define DHT11PIN 4           // Temp. & Hum.
+#define DHT_PIN 1  // Temp. & Hum.
+#define DHTTYPE DHT11  
+
+DHT dht(DHT_PIN, DHTTYPE);
 
 //Ultrasonic Sensor
 const int trigPin = 8;
 const int echoPin = 9;
 
-
-// int a=0; 
-// int i=10; 
-// int n=0; 
-
 bool startMessung = false; 
+bool startValves = false;
 
-// dht11 DHT11;
-
-// float temp;
-// float hum; 
+int drinkChoice = 0;
+ 
+float temp;
+float hum; 
 float alkVolt;
 float alkVoltMax=0; 
 int distance; 
-float distanceMid; // Mittelwert
+
+// Mittelwerte
+float distanceMid; 
+float tempMid;
+float humMid;
 
 // skalierte Werte
-//int tempMidBit;
-//int humMidBit;
+int tempMidBit;
+int humMidBit;
 int distanceMidBit;
 int alkVoltMaxBit;
 
-byte data[3];
-
+byte data[5];
 
 void setup() 
 {
@@ -44,6 +46,8 @@ void setup()
   pinMode(PIN_ANALOG_OUT, INPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);  
+  pinMode(DHT_PIN, INPUT);
+  dht.begin();
 }
 
 
@@ -54,12 +58,21 @@ void loop()
   {
     alkVolt = 0;
     alkVoltMax = 0;
+  
     distance = 0;
     distanceMid = 0;
-    data[0] = 0;
+  
+    temp = 0;
+    tempMid = 0;
+   
+    hum = 0;
+    humMid = 0;
+
+    data[0] = 99; // SPEICHER-REGISTER ADRESSE?
     data[1] = 0;
-    // temp = 0;
-    // hum = 0;
+    data[2] = 0;
+    data[3] = 0;
+    data[4] = 0;
 
     // MESSUNG FÜR 10 SEKUNDEN
     for (int i = 0; i <= 9; i++)
@@ -73,10 +86,11 @@ void loop()
         alkVoltMax=alkVolt;
       }
 
-      // Aufaddieren der Abstandswerte
+      // Aufaddieren der Abstands-, Temperatur und Feuchtigkeitswerte
       distance = distance + readUltrasonic();
-      // temp = readTemp()+temp;
-      // hum = readHum()+hum;
+      temp = temp + dht.readTemperature();
+      hum = hum + dht.readHumidity();
+
 
       Serial.print("Distance:");
       Serial.println(distance);
@@ -87,12 +101,13 @@ void loop()
       Serial.print("max. Alk: ");
       Serial.println(alkVoltMax);
       
-      // Serial.print("Temp: ");
-      // Serial.println(temp);
-      // Serial.print("Hum: ");
-      // Serial.println(hum);
+      Serial.print("Temp: ");
+      Serial.println(temp);
+      
+      Serial.print("Hum: ");
+      Serial.println(hum);
 
-      delay(950); 
+      delay(800); 
     }
 
     // AUSGABE DER MESSERGEBNISSE
@@ -103,12 +118,20 @@ void loop()
     Serial.print("nach 10 Sekunden Messung --> Mittelwert des Abstandssensor:");
     Serial.println(distanceMid);
 
+    tempMid = temp/10;
+    Serial.print("nach 10 Sekunden Messung --> Mittelwert des Temperatursensor:");
+    Serial.println(tempMid);
+
+    humMid = hum/10;
+    Serial.print("nach 10 Sekunden Messung --> Mittelwert des Feuchtigkeitssensor:");
+    Serial.println(humMid);
+
 
     // SKALIEREN DER WERTE AUF WERTEBEREICH 0 - 255  
     alkVoltMaxBit = (alkVoltMax/5) * 255; 
     distanceMidBit = (distanceMid/50) * 255; 
-    // tempMidBit = (tempMid/40) * 255;  
-    // humMidBit = (humMid/100) * 255; 
+    tempMidBit = (tempMid/40) * 255;  
+    humMidBit = (humMid/100) * 255; 
     
    //Abstandsmessung wird nach einigen 50-100cm fehlerhaft(zu große Werte, über 20m) deswegen skalieren auf Max Wert
    if (distanceMidBit > 255)
@@ -123,18 +146,33 @@ void loop()
     Serial.print("Skalierter Abstandswert:");
     Serial.println(distanceMidBit);
   
-    // Serial.print("Skalierter Temperaturwert:");
-    // Serial.println(tempMidBit);
+    Serial.print("Skalierter Temperaturwert:");
+    Serial.println(tempMidBit);
   
-    // Serial.print("Skalierter Feuchtigkeitswert: ");
-    // Serial.println(humMidBit);
+    Serial.print("Skalierter Feuchtigkeitswert: ");
+    Serial.println(humMidBit);
 
-    data[0] = 99; // REGISTER ADRESSE?
-    data[1] = distanceMidBit;
-    data[2] = alkVoltMaxBit;
+    data[1] = alkVoltMaxBit;
+    data[2] = distanceMidBit;
+    data[3] = tempMidBit;
+    data[4] = humMidBit;
     
     startMessung = false;   
-  }    
+  }
+  
+  if (startValves == true) 
+  {
+    Serial.print("Ausgewählter Drink:");
+    Serial.println(drinkChoice);
+
+    // INSERT DRINK ROUTINE
+
+    //
+
+    //
+
+    startValves = false;
+  }
 }
 
 
@@ -169,39 +207,25 @@ int readUltrasonic()
   return(distance);
 }
 
-// float readTemp()
-// {
-//  int chk = DHT11.read(DHT11PIN);
-//  float temper = DHT11.temperature;
-
-//  return(temper);
-// }
-
-// float readHum()
-// {
-//  int chk = DHT11.read(DHT11PIN);
-//  float humi = DHT11.humidity;
-
-//  return (humi); 
-// }
-
-
 // I2C FUNCTIONS
 
 void receiveEvent(int howMany) {
     int rxData = Wire.read();
     
-    if (rxData == 11) 
-    {
+    if (rxData == 11) {
       startMessung = true; 
-    } 
-  
+    } else if (rxData < 11) {
+      startValves = true;
+      drinkChoice = rxData; 
+    }
 }
 
 void sendData() { 
-    Wire.write(data, 3);    
+    Wire.write(data, 5);    
     Serial.println("Daten versendet");
     Serial.println(data[0]);
     Serial.println(data[1]);
     Serial.println(data[2]);
+    Serial.println(data[3]);
+    Serial.println(data[4]);
 }
